8db7a1cec57eed06ce0b4768406b18542344d8a7
Łukasz Sewielski <sewielski.lukasz@gmail.com>
2025-11-29 17:33:11 +0100

    Integration between merges

--- scripts/Beacon.gd
+++ scripts/Beacon.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var collider = $CollisionShape2D
 @export var cell_size: float = 24
 @export var radius: float = 72
 @export var color: Color = Color(0.73, 0.35, 0.11, 0.5)
@@ -8,12 +9,13 @@ var time_modifier = 0;
 
 func _ready():
 	
-	var collider_poly = generate_circle_collider(32, 32*2)
+	var collider_poly = generate_circle_collider(128, 128*2)
 	
 	# Draw Polygon2D to visualize
 	var poly = Polygon2D.new()
 	poly.polygon = collider_poly
 	poly.color = color
+	#collider.shape = poly
 	add_child(poly)
 
 func generate_circle_collider(cell_size: float, radius: float) -> PackedVector2Array:
