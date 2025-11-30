0a914867d14c1f38da13cc35500306b8de548ef0
Łukasz Sewielski <sewielski.lukasz@gmail.com>
2025-11-30 10:24:56 +0100

    Added transitions between levels

---
+++ scripts/epic_level_2.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var enemy = get_tree().get_nodes_in_group("enemies")
+	if enemy.is_empty():
+		var new_scene = load("res://level/EpicLevel3.tscn")
+		get_tree().change_scene_to_packed(new_scene)
+	pass
