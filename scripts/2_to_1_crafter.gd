54f0ab2944ebe3ae3abfb454928408255d968bc2
Patryk Januś <patryk.jap@gmail.com>
2025-11-30 03:42:02 +0100

    added details

--- scripts/2_to_1_crafter.gd
+++ scripts/2_to_1_crafter.gd
@@ -7,10 +7,11 @@ extends Node2D
 @export var second_resource_cost : int = 1
 @export var beacon_manager: Node2D
 
-@onready var first_label = $"Top cloud/First input"
-@onready var second_label = $"Top cloud/Second input"
-@onready var output_label = $"Bot cloud/Output"
+@onready var first_label = $"gui/Top cloud/First input"
+@onready var second_label = $"gui/Top cloud/Second input"
+@onready var output_label = $"gui/Bot cloud/Output"
 @onready var timer = $Timer
+@onready var building_animator = $"Building animation"
 
 var first_counter = 0
 var second_counter = 0
@@ -21,6 +22,7 @@ var can_take = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	building_animator.speed_scale = 0
 	first_generator.increase_nodes()
 	second_generator.increase_nodes()
 	pass # Replace with function body.
@@ -43,12 +45,17 @@ func _process(delta: float) -> void:
 	output_label.text = str(output_counter)
 	var beacon_time_multipier = beacon_manager.get_time_modifier_at_world(self.position)
 	if beacon_time_multipier == 0 and not timer.is_stopped():
+		building_animator.speed_scale = 0
 		timer.stop()
 	elif beacon_time_multipier != 0 and timer.is_stopped():
 		timer.wait_time = (1 / beacon_time_multipier) * production_time
 		timer.start()
+		building_animator.speed_scale = (1/timer.wait_time) * 6
 	elif beacon_time_multipier != 0 :
 		timer.wait_time = (1 / beacon_time_multipier) * production_time
+		building_animator.speed_scale = (1/timer.wait_time) * 6
+		
+		
 	if output_counter <= 0:
 		can_take = false
 	if first_generator.can_take:
